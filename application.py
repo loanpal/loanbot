b6668eb4de07e3343e20aa7a6a558c8c707a980e
Allan Carroll <allanca@gmail.com>
2016-08-01 22:01:17 -0700

    Add more message parsing

--- application.py
+++ application.py
@@ -22,56 +22,113 @@ def verify():
 
 @application.route('/', methods=['POST'])
 def webook():
-
-    # endpoint for processing incoming messaging events
-
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    if data["object"] == "page":
+    if data["object"] != "page":
+        return "ok", 200
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "got it, thanks!")
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+    for entry in data["entry"]:
+        for messaging_event in entry["messaging"]:
+            if messaging_event.get('message'):
+              received_message(messaging_event)
+            # elif messaging_event.get('optin'):
+            #   receivedAuthentication(messaging_event)
+            # elif messaging_event.get('delivery'):
+            #   receivedDeliveryConfirmation(messaging_event)
+            # elif messaging_event.get('postback'):
+            #   receivedPostback(messaging_event)
+            # elif messaging_event.get('read'):
+            #   receivedMessageRead(messaging_event)
+            # elif messaging_event.get('account_linking'):
+            #   receivedAccountLink(messaging_event)
+            else:
+              log("Webhook received unknown messaging_event: " + json.dumps(messaging_event))
 
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
+#
+# Message Event
+#
+# This event is called when a message is sent to your page. The 'message'
+# object format can vary depending on the kind of message that was received.
+# Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+#
+# For this example, we're going to echo any text that we get. If we get some
+# special keywords ('button', 'generic', 'receipt'), then we'll send back
+# examples of those bubbles to illustrate the special message bubbles we've
+# created. If we receive a message with an attachment (image, video, audio),
+# then we'll simply confirm that we've received the attachment.
+#
+def received_message(event):
+    sender_id = event['sender']['id']
+    recipient_id = event['recipient']['id']
+    time_of_message = event['timestamp']
+    message = event['message']
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    log("Received message for user %s and page %s at %d with message:" %
+            (sender_id, recipient_id, time_of_message))
+    log(json.dumps(message));
 
+    is_echo = message.get('is_echo')
+    message_id = message.get('mid')
+    sequence_id = message.get('seq')
+    sticker_id = message.get('sticker_id')
+    app_id = message.get('app_id')
+    metadata = message.get('metadata')
+
+    # You may get a text or attachment but not both
+    message_text = message.get('text')
+    message_attachments = message.get('attachments')
+    quick_reply = message.get('quick_reply')
+
+    if is_echo:
+        # Just logging message echoes to console
+        log("Received echo for message %s and app %s with metadata %s" %
+                (message_id, app_id, metadata));
+        return
+
+    elif quick_reply:
+        quick_reply_payload = quick_reply['payload'];
+        log("Quick reply for message %s with payload %s" %
+                (message_id, quick_reply_payload))
+        send_text_message(senderID, "Quick reply tapped");
+        return
+
+    if message_text:
+        send_text_message(sender_id, message_text)
+    elif message_attachments:
+        send_text_message(sender_id, "Message with attachment received")
+    else:
+        log("Invalid message")
+
+
+def send_text_message(recipient_id, message_text):
+    log("sending message to {recipient}: {text}"
+            .format(recipient=recipient_id, text=message_text))
+
+    send_message({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text,
+        }
+    })
+
+
+def send_message(data):
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
     headers = {
         "Content-Type": "application/json"
     }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    data = json.dumps(data)
+
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+            params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
